5414f87e0c6bb80219a3ac573fb80918d8e9fcb9
Jake Weston <jweston491@gmail.com>
2021-05-24 11:05:07 -0700

    init commit

---
+++ functions/sendgrid.js
@@ -0,0 +1,58 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail')
+const ipn = require('paypal-ipn');
+const querystring = require("querystring");
+
+const {
+    SENDGRID_API_KEY,
+    SENDGRID_TO_EMAIL,
+    SENDGRID_FROM_EMAIL,
+  } = process.env;
+
+exports.handler = async (event, context) => {
+    // Only allow POST
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+  
+    // When the method is POST, the name will no longer be in the event’s
+    // queryStringParameters – it’ll be in the event body encoded as a query string
+    const params = querystring.parse(event.body);
+  
+    ipn.verify(params, {'allow_sandbox': true}, function callback(err, msg) {
+        if (err) {
+          console.error(err);
+        } else {
+       
+          if (params.payment_status == 'Completed') {
+            // Payment has been confirmed as completed
+            sgMail.setApiKey(SENDGRID_API_KEY)
+            const msg = {
+                to: 'jweston491@gmail.com', // Change to your recipient
+                from: 'jacob.weston@wsu.edu', // Change to your verified sender
+                subject: params.first_name + ' ' + params.last_name + ' registered for ' + params.item_name,
+                text: params.first_name + ' ' + params.last_name + ' registered for ' + params.item_name + '.',
+                html: '<p>Contact Info:<br/>Name: ' + params.first_name + ' ' + params.last_name + '<br/>Email: ' + params.payer_email + '<br/>Phone: ' + params.contact_phone + '</p><p>Address:<br/>' + params.address_street + '<br/>' + params.address_city + '<br/>' + params.address_state + ' ' + params.address_zip + ', ' + params.address_country + '</p>',
+            }
+            sgMail
+            .send(msg)
+            .then(() => {
+                console.log('Email sent')
+                const redirectUrl = 'https://example.com'
+                return {
+                statusCode: 302,
+                headers: {
+                    Location: redirectUrl,
+                    'Cache-Control': 'no-cache',
+                },
+                body: JSON.stringify({})
+                }
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+          }
+        }
+      });
+  };
