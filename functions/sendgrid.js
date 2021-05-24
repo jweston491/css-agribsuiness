097ac0a75ea9bbe7656834f8804555e23027b4e4
Jake Weston <jweston491@gmail.com>
2021-05-24 12:13:59 -0700

    Update sendgrid script

--- functions/sendgrid.js
+++ functions/sendgrid.js
@@ -19,6 +19,8 @@ exports.handler = async (event, context) => {
     // When the method is POST, the name will no longer be in the event’s
     // queryStringParameters – it’ll be in the event body encoded as a query string
     const params = querystring.parse(event.body);
+
+    console.log(ipn)
   
     ipn.verify(params, {'allow_sandbox': true}, function callback(err, msg) {
         if (err) {
